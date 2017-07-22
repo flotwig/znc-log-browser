31e808f6fe93d604364d2b32d03317bcb205b46d
Zach Bloomquist <zbloomq@live.com>
2017-07-22 18:01:24 -0500

    Breadcrumbs; initial log viewer

--- logbrowser.py
+++ logbrowser.py
@@ -1,8 +1,8 @@
 # logbrowser.py
 
 import znc
-import os
 from glob import glob
+from urllib import parse
 
 
 class logbrowser(znc.Module):
@@ -15,27 +15,27 @@ class logbrowser(znc.Module):
     def OnWebRequest(self, sock, page, tmpl):
         if str(page) == 'log':
             return self._OnLogRequest(sock, page, tmpl)
-        else:
+        elif str(page) == 'index':
             return self._OnIndexRequest(sock, page, tmpl)
-
-    def _OnLogRequest(self, sock, page, tmpl):
-        return True
+        else:
+            return sock.PrintNotFound()
 
     def _OnIndexRequest(self, sock, page, tmpl):
         network_param = str(sock.GetParam('network', False))
         channel_param = str(sock.GetParam('channel', False))
+        network = ''
+        channel = ''
         networks = self._GetNetworks(sock)
         if len(networks) == 1:
             network = networks[0]
         elif network_param in networks:
             network = network_param
-        else:
-            network = ''
         tmpl["Network"] = str(network)
         for network_name in networks:
             row = tmpl.AddRow("Networks")
             row["Name"] = str(network_name)
             row["Selected"] = str(str(network_name) == network)
+            row['URL'] = self._GetLink(network_name)
         # load channels for network
         if network != '':
             channels = self._GetChannels(sock, network)
@@ -43,14 +43,14 @@ class logbrowser(znc.Module):
                 channel = channels[0]
             elif channel_param in channels:
                 channel = channel_param
-            else:
-                channel = ''
             tmpl["Channel"] = str(channel)
+            bc = tmpl.AddRow("BreadCrubs")
             for channel_name in channels:
                 row = tmpl.AddRow("Channels")
                 row["Name"] = str(channel_name)
                 row["Selected"] = str(str(channel_name) == channel)
                 row["Network"] = network
+                row['URL'] = self._GetLink(network, channel_name)
         # load days for channel
         if network != '' and channel != '':
             days = self._GetDays(sock, network, channel)
@@ -59,7 +59,44 @@ class logbrowser(znc.Module):
                 row["Name"] = str(day)
                 row["Network"] = network
                 row["Channel"] = channel
+                row['URL'] = self._GetLink(network, channel, day)
+        self._SetBreadCrumbs(tmpl, network, channel)
         return True
+
+    def _OnLogRequest(self, sock, page, tmpl):
+        network_param = str(sock.GetParam('network', False))
+        channel_param = str(sock.GetParam('channel', False))
+        day_param = str(sock.GetParam('day', False))
+        networks = self._GetNetworks(sock)
+        if not network_param in networks:
+            return sock.PrintNotFound()
+        channels = self._GetChannels(sock, network_param)
+        if not channel_param in channels:
+            return sock.PrintNotFound()
+        days = self._GetDays(sock, network_param, channel_param)
+        if not day_param in days:
+            return sock.PrintNotFound()
+        with self._OpenLog(sock, network_param, channel_param,
+                           day_param) as log:
+            for line in log:
+                row = tmpl.AddRow("LogLines")
+                row["Message"] = line
+        self._SetBreadCrumbs(tmpl, network_param, channel_param, day_param)
+        return True
+
+    def _SetBreadCrumbs(self, tmpl, network, channel, day=''):
+        if network != '':
+            row = tmpl.AddRow("BreadCrumbs")
+            row['Text'] = str(network)
+            row['URL'] = self._GetLink(network)
+        if network != '' and channel != '':
+            row = tmpl.AddRow("BreadCrumbs")
+            row['Text'] = str(channel)
+            row['URL'] = self._GetLink(network, channel)
+        if network != '' and channel != '' and day != '':
+            row = tmpl.AddRow("BreadCrumbs")
+            row['Text'] = 'Log for ' + str(day)
+            row['URL'] = self._GetLink(network, channel, day)
 
     def _GetNetworks(self, sock):
         dirs = map(self._GetLastDir, glob(self._GetLogPath(sock) + '*/'))
@@ -70,11 +107,27 @@ class logbrowser(znc.Module):
                    glob(self._GetLogPath(sock) + network + '/*/'))
         return list(dirs)
 
+    def _GetLink(self, network='', channel='', day=''):
+        url = '?' + parse.urlencode({
+            'network': network,
+            'channel': channel,
+            'day': day
+        })
+        if day != '':
+            url = 'log' + url
+        url = self.GetWebPath() + url
+        return url
+
     def _GetDays(self, sock, network, channel):
         files = map(
             self._GetLogName,
             glob(self._GetLogPath(sock) + network + '/' + channel + '/*'))
         return files
+
+    def _OpenLog(self, sock, network, channel, day):
+        return open(
+            self._GetLogPath(sock) + network + '/' + channel + '/' + day +
+            '.log')
 
     def _GetLastDir(self, dir):
         return dir.split('/')[-2]
